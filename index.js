260faf4c7451f011b1cdf27a9d22ad4804f0bb44
yeasin-hossain <shantoxdp@gmail.com>
2020-07-06 13:50:24 +0600

    bus seats add

--- index.js
+++ index.js
@@ -8,13 +8,36 @@ app.get('/',(req,res)=>{
 app.get('/data',(req,res)=>{
     
     const userData = {
-        name:'Shanto',
-        pro:'Developer'
+        "Name":'Shanto'
     }
 
     res.send(userData);
 });
+// bus seats
 
+app.get('/seat',(req,res)=>{
+    const busSeats = {
+        "id": 2,
+        "trip_id": 3,
+        "a1": 1,
+        "a2": 0,
+        "a3": 1,
+        "a4": 0,
+        "b1": 1,
+        "b2": 0,
+        "b3": 1,
+        "b4": 0,
+        "c1": 1,
+        "c2": 1,
+        "c3": 0,
+        "c4": 0,
+        "d1": 1,
+        "d2": 0,
+        "d3": 0,
+        "d4": 0
+    }
+    res.send(busSeats);
+});
 // 404 error
 app.get('*',(req,res)=>{
     res.send('Sorry Not Found 404!');
